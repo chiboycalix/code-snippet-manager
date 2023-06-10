562d62f70b05909384e6b91c9b26d7bbb2825e53
chiboycalix <igwechinonso77@gmail.com>
2023-06-10 21:29:34 +0100

    add automatic deploy to ec2

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Stop the existing Go server
+systemctl stop code-snippet-manager
+
+# Navigate to your Go server directory
+cd /home/ec2-user/projects/code-snippet-manager
+
+# Pull the latest changes from the GitHub repository
+git pull
+
+# Build the Go server application
+go build
+
+# Restart the Go server
+systemctl start /home/ec2-user/projects/code-snippet-manager
